7c583cbc73c07364369b1baea38e172137d080e3
Tanishkagupta123 <tanishkagupta241@gmail.com>
2026-01-13 03:57:20 +0530

    react

--- src/NewForm.jsx
+++ src/NewForm.jsx
@@ -2,47 +2,44 @@ import React, { useState } from 'react'
 
 const NewForm = () => {
 
-      let [myname,setmyname]=useState('')
-      let [city,setcity]=useState('')
+    //   let [myname,setmyname]=useState('')
+    //   let [city,setcity]=useState('')
+    //   let [state,setstate]=useState('')
 
+    let [form,setform] =useState({
+        myname:'',
+        city:'',
+        state:''
+    })
 
     let handlechange=(e)=>{
-
-        console.log(e.target.value);
-        setmyname(e.target.value)
-        
+        setform({...form, [e.target.name]:e.target.value})
     }
 
-    let handle=(e)=>{
-
-        console.log(e.target.value);
-        setcity(e.target.value)
-
-
-    }
+    // let handle=(e)=>{
+    //     console.log(e.target.value);
+    //     setcity(e.target.value)
+    // }
 
   return (
-
     <>
-    <h1>Form Handleing</h1>
+      <h1>Form Handleing</h1>
 
-    Enetr name:   <input type="text" onChange={handlechange} /><br /><br />
-    Enter city: <input type="text" onChange={handle} /><br /><br /><br />
+      Enetr name:
+      <input type="text" name='myname' value={form.myname} onChange={handlechange} />
+      <br /><br />
 
+      Enter city:
+      <input type="text" name='city' value={form.city} onChange={handlechange} />
+      <br /><br /><br />
 
+      Enter state:
+      <input type="text" name='state' value={form.state} onChange={handlechange} />
+      <br /><br />
 
-
-
-   my name is  <h1>{myname}</h1> and
-  my age is  <h1>{city}</h1>
-
-
-
-
-
-
-
-
+      my name is <h1>{form.myname}</h1>
+      and my age is <h1>{form.city}</h1>
+      and my state is <h1>{form.state}</h1>
     </>
   )
 }
